ba1fde8c3ddee3ddccbe093d841e8ec92c15b46c
Joshua Moon <jcmoon@pm.me>
2025-11-25 18:11:14 -0500

    Direct global flag ability for moderators.

--- XF/ControllerPlugin/ReportPlugin.php
+++ XF/ControllerPlugin/ReportPlugin.php
@@ -4,6 +4,7 @@ namespace USIPS\NCMEC\XF\ControllerPlugin;
 
 use XF\Mvc\Entity\Entity;
 use XF\Service\Report\CreatorService;
+use USIPS\NCMEC\Service\Incident\ModeratorFlagger;
 
 class ReportPlugin extends XFCP_ReportPlugin
 {
@@ -30,6 +31,28 @@ class ReportPlugin extends XFCP_ReportPlugin
 
     public function actionReport($contentType, Entity $content, $confirmUrl, $returnUrl, $options = [])
     {
+        if ($this->request->isPost() && $this->request->filter('report_type', 'str') === 'flag_as_csam')
+        {
+            if (!\XF::visitor()->is_moderator)
+            {
+                throw $this->exception($this->controller->noPermission());
+            }
+
+            /** @var ModeratorFlagger $flagger */
+            $flagger = $this->service('USIPS\\NCMEC:Incident\\ModeratorFlagger', $content);
+            $incident = $flagger->flag();
+
+            if (!$incident)
+            {
+                throw $this->exception($this->controller->error(\XF::phrase('usips_ncmec_unable_to_flag_content')));
+            }
+
+            return $this->controller->redirect(
+                $this->app->router('admin')->buildLink('ncmec-incidents/view', $incident),
+                \XF::phrase('usips_ncmec_report_flagged_as_csam')
+            );
+        }
+
         $options = array_merge([
             'view' => 'XF:Report\\Report',
             'template' => 'usips_ncmec_report_create',
